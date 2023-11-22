516afadcc351148bbf3299aa9ed8476b7fac2bf6
Siri <xd42069xd69420@gmail.com>
2023-11-22 17:38:33 +0900

    made an entirely untested directory walker
    would use Pattern<'a> but its unstable on stable rust rn so no.

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use std::{fs, io, path};
+
+pub type RecurseResult<T> = Result<T, RecurseError>;
+
+pub enum RecurseError {
+    NotADirectory,
+    IOErr(io::Error)
+}
+
+pub fn recurse_find(root_dir: path::PathBuf, condition: fn (&str) -> bool) -> RecurseResult<Vec<fs::File>> 
+{
+    let mut matches: Vec<fs::File> = Vec::new();
+    if !root_dir.is_dir() {
+        return Err(RecurseError::NotADirectory)
+    }
+    let readdir = match root_dir.read_dir() {
+        Ok(readdir) => readdir,
+        Err(err) => return Err(RecurseError::IOErr(err))
+    };
+    for entry in readdir {
+        let path = match entry {
+            Ok(entry) => entry.path(),
+            Err(err) => return Err(RecurseError::IOErr(err))
+        };
+        if path.is_dir() {
+            match recurse_find(root_dir.clone(), condition) {
+                Ok(mut vec) => matches.append(&mut vec),
+                Err(e) => return Err(e)
+            }
+        } else if let Some(name) = path.file_name() {
+            if let Some(name) = name.to_str() { 
+                if condition(name) {
+                    matches.push(
+                        match fs::File::open(path) {
+                            Ok(f) => f,
+                            Err(err) => return Err(RecurseError::IOErr(err))
+                        }
+                    )
+                }
+            }
+        }
+    }
+    return Ok(matches);
+}
+
