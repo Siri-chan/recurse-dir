a4d1356272b2746588fb4de636be841f83a02676
Siri <xd42069xd69420@gmail.com>
2023-11-22 17:42:40 +0900

    made the condition take a pathbuf instead

--- src/lib.rs
+++ src/lib.rs
@@ -4,42 +4,37 @@ pub type RecurseResult<T> = Result<T, RecurseError>;
 
 pub enum RecurseError {
     NotADirectory,
-    IOErr(io::Error)
+    IOErr(io::Error),
 }
 
-pub fn recurse_find(root_dir: path::PathBuf, condition: fn (&str) -> bool) -> RecurseResult<Vec<fs::File>> 
-{
+pub fn recurse_find(
+    root_dir: path::PathBuf,
+    condition: fn(&path::PathBuf) -> bool,
+) -> RecurseResult<Vec<fs::File>> {
     let mut matches: Vec<fs::File> = Vec::new();
     if !root_dir.is_dir() {
-        return Err(RecurseError::NotADirectory)
+        return Err(RecurseError::NotADirectory);
     }
     let readdir = match root_dir.read_dir() {
         Ok(readdir) => readdir,
-        Err(err) => return Err(RecurseError::IOErr(err))
+        Err(err) => return Err(RecurseError::IOErr(err)),
     };
     for entry in readdir {
         let path = match entry {
             Ok(entry) => entry.path(),
-            Err(err) => return Err(RecurseError::IOErr(err))
+            Err(err) => return Err(RecurseError::IOErr(err)),
         };
         if path.is_dir() {
             match recurse_find(root_dir.clone(), condition) {
                 Ok(mut vec) => matches.append(&mut vec),
-                Err(e) => return Err(e)
+                Err(e) => return Err(e),
             }
-        } else if let Some(name) = path.file_name() {
-            if let Some(name) = name.to_str() { 
-                if condition(name) {
-                    matches.push(
-                        match fs::File::open(path) {
-                            Ok(f) => f,
-                            Err(err) => return Err(RecurseError::IOErr(err))
-                        }
-                    )
-                }
-            }
+        } else if condition(&path) {
+            matches.push(match fs::File::open(path) {
+                Ok(f) => f,
+                Err(err) => return Err(RecurseError::IOErr(err)),
+            })
         }
     }
     return Ok(matches);
 }
-
